bcd10a56aab767856d6895d47ba2f2c4eea44f08
Roxxik <Roxxik@users.noreply.github.com>
2015-04-28 15:07:16 +0200

    Update samegame2-1.py

--- samegame2-1.py
+++ samegame2-1.py
@@ -17,9 +17,11 @@ class Game(object):
             (3, 2, 2, 4, 4, 1, 3, 4, 1, 3), 
             (1, 2, 2, 4, 4, 2, 3, 1, 3, 3), 
             (2, 3, 4, 1, 3, 4, 3, 2, 4, 1),)
+  
   def click(self,col,row):
     pts, self.board = self.floodfill(col,row, self.board[row][col], self.board)
     self.board = self.gravity(self.board)
+    self.board = self.move(self.board)
     return pts
     
   
@@ -33,13 +35,17 @@ class Game(object):
     points3, board = self.floodfill(col+1, row, color, board)
     points4, board = self.floodfill(col-1, row, color, board)
     return sum([points1,points2,points3,points4]) + 1, board
-  
+
   def gravity(self,board):
     return zip(*map(self.shift,zip(*board)))
     
   def shift(self, line):
     numbers = filter(bool,line)
     return tuple([0]*(len(line) - len(numbers)) + list(numbers))
+
+  def move(self, board):
+    cols = filter(lambda line: any(map(bool,line)),zip(*board))
+    return tuple(zip(*(([0]*self.rows)*(len(cols)-self.cols)) + cols))
     
   def display(self):
     print "\n".join(map(lambda l:" ".join(map(str,l)), self.board))
