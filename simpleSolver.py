f5c9cb07cab4a9a1edd33663460f015218a7acf7
Roxxik <Roxxik@users.noreply.github.com>
2015-04-29 20:31:36 +0200

    started a simpleSolver to test the gameLoop

--- simpleSolver.py
+++ simpleSolver.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
+from board import floodfill
+
+def parseBoard():
+  pass
 
 def main():
-  pass
+  board = parseBoard()
+  moves = []
+  rows = len(board)
+  cols = len(board[0])
+  while(1):
+    madeMove = False
+    for x in range(cols):
+      for y in range(rows):
+        pts, newBoard = floodfill(board,board[x][y],x,y)
+        if pts > 1:
+          move.append((x,y))
+          board = newBoard
+          madeMove = True
+          break
+      if madeMove:
+        break
+    if not madeMove:
+      break
+  printMoves(moves)
+  
 
 if __name__ == "__main__":
   main()
