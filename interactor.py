3e545bce8dd1b4ad2988f8c173cd648f0eb0cf90
Roxxik <Roxxik@users.noreply.github.com>
2015-04-29 22:17:05 +0200

    improved interactor

--- interactor.py
+++ interactor.py
@@ -3,14 +3,22 @@ from subprocess import Popen, PIPE
 from board import Board
 from validator import getScore
 
+def interact(prog, board):
+  try:
+    proc = Popen(prog,stdin=PIPE,stdout=PIPE,shell=True)
+    proc.stdin.write(str(board) + "\n")
+    proc.wait()
+    return eval(proc.stdout.read())
+  except:
+    return None  
+
 def main():
   board = Board(colorDistribution = [1,1,1,1],seed=0xDEADBEEF).board
-  proc = Popen("python simpleSolver.py",stdin=PIPE,stdout=PIPE,shell=True)
-  proc.stdin.write(str(board) + "\n")
-  proc.wait()
-  output = proc.stdout.read()
-  moves = eval(output)
-  print getScore(board,moves)
+  moves = interact("python simpleSolver.py",board)
+  if moves != None:
+    print getScore(board,moves)
+  else:
+    print "error"
 
 if __name__ == "__main__":
   main()
